b505462ec0aafe1122fcb77ba8f765b9589d7f41
Sergey Shakhovskiy <aultero@gmail.com>
2022-11-02 20:50:07 +0100

    Extend templates

--- app.py
+++ app.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+exchanges =[
+    {
+        'name': 'Binance',
+        'logo': '/static/img/binance.svg'
+    },
+    {
+        'name': 'Poloniex',
+        'logo': '/static/img/poloniex.svg'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', title = 'Main page')
 
 @app.route("/settings")
 def settings():
-    return render_template('settings.html')
+    return render_template('settings.html', exchanges = exchanges, title = 'Settings')
