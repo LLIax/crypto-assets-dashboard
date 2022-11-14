121c8670396ebdf937837347577ab59e229c5da7
Sergey Shakhovskiy <aultero@gmail.com>
2022-11-14 21:21:25 +0100

    Add binance balances

--- cryass/crontasks.py
+++ cryass/crontasks.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from cryass import app, db, crontab
 from binance import Client
 from datetime import datetime
+from binance.exceptions import BinanceAPIException
 
 
 @crontab.job(minute="20", hour="18")
@@ -26,38 +27,23 @@ def scheduled_job():
     with open('readme.txt', 'w') as f:
         f.write('readme')
 
+    # list of assets not to  be dusted
+    assets = ('BNB', 'BTC', 'BCH', 'USDT', 'BUSD', 'TRX', 'DOGE', 'NFT')
 
+    dust = ""
 
-from binance.exceptions import BinanceAPIException
+    client = Client(api_key, api_secret)
 
-api_key = "8IfPZfCcZzvQfjVpIEOfRPlDIGgDH89OHuSXD640OhVhblgM1aPGy2LwgesoiQZu"
-api_secret = "oirrqn8TRtXs7tlULtoqutx8KYf7CkfX3Lg75cdhWKs9UQ76eyElM9UvoEzVTsFJ"
+    info = client.get_account()
+    print()
+    print(str(datetime.date(datetime.now())) + " * Binance")
+    for balance in info["balances"]:
+        if float(balance["free"]) > 0:
+            # listing of coins on Earn wallet to ledger file
+            balance = Balance(name="Binance", account="free", currency=balance["asset"], balance=balance["free"])
+            db.session.add(balance)
 
-# list of assets not to  be dusted
-assets = ('BNB', 'BTC', 'BCH', 'USDT', 'BUSD', 'TRX', 'DOGE', 'NFT')
-
-dust = ""
-
-client = Client(api_key, api_secret)
-
-info = client.get_account()
-print()
-print(str(datetime.date(datetime.now())) + " * Binance")
-for balance in info["balances"]:
-    if float(balance["free"]) > 0 or float(balance["locked"]) >0:
-        # listing of coins on Earn wallet to ledger file
-        if balance["asset"].startswith("LD"):
-            print("    assets:crypto:binance   = " + balance["asset"][2:]+ " " + balance["free"])
-        elif balance["asset"] not in assets:
-            if dust:
-                dust = dust + ", "
-            dust = dust + balance["asset"]
-print("    income:crypto:interest")
-if dust:
-    try:
-        cmdbnb = str('(asset="' + dust + '")')
-        cmdbnb = "client.transfer_dust" + cmdbnb
-        #print(cmdbnb)
-        result = exec(cmdbnb)
-    except:
-        print ("; error dedusting"+ str(result))
+        if float(balance["locked"]) >0:
+            balance = Balance(name="Binance", account="free", currency=balance["asset"], balance=balance["free"])
+            db.session.add(balance)
+    db.session.commit()
